552e72cf8bb7be15b9c0da091bed3b44222cba0b
wxul1a <105665903+wxul1a@users.noreply.github.com>
2022-11-23 17:01:12 -0300

    Add files via upload

---
+++ temperatura/js/temperatura.js
@@ -0,0 +1,10 @@
+let cel = document.getElementById("cel");
+let far = document.getElementById("far");
+let kel = document.getElementById("kel");
+let ran = document.getElementById("kel");
+
+
+document.getElementById("cover").addEventListener("click", () => {
+    let valueCel = parseInt(valueCel.value);
+    let cover = valueCel / 5 * 
+});
